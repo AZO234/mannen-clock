ee8780aa1e19c174ec9df35024b992fb63070dd5
AZO234 <typesylph@gmail.com>
2026-03-13 01:17:03 +0900

    1.0.19

--- src/App.vue
+++ src/App.vue
@@ -59,19 +59,35 @@ function setFontSize(s: FontSize) {
     </div>
   </header>
 
-  <div>
-    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">ポスト</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2FAZO234.github.com%2Fmannen-clock&layout&size&width=93&height=20&appId" width="93" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-    <div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL" data-url="https://azo234.github.io/mannen-clock/" data-color="default" data-size="small" data-count="false" data-ver="3" style="display: none;"></div><script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script></div>
-    <div>
+  <section class="section">
+    <div class="sns-buttons">
+      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+         class="twitter-share-button"
+         data-show-count="false">ポスト</a>
+
+      <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2FAZO234.github.io%2Fmannen-clock&layout&size&width=93&height=20&appId"
+        width="93" height="20"
+        style="border:none;overflow:hidden"
+        scrolling="no" frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
+      <div class="line-it-button"
+        data-lang="ja" data-type="share-a" data-env="REAL"
+        data-url="https://azo234.github.io/mannen-clock/"
+        data-color="default" data-size="small"
+        data-count="false" data-ver="3"
+        style="display: none;"></div>
+
       <a href="https://coff.ee/azo234" target="_blank" rel="noopener noreferrer">
         <img src="https://img.shields.io/badge/-Buy%20Me%20a%20Coffee-ffdd00?style=flat-square&logo=buymeacoffee&logoColor=black" alt="Buy Me a Coffee">
       </a>
+
       <a href="https://github.com/sponsors/azo234" target="_blank" rel="noopener noreferrer">
-        <img src="https://img.shields.io/static/v1?label=Sponsor&amp;message=❤️&amp;logo=github-sponsors&amp;color=lightgrey&amp;style=for-the-badge" alt="Sponsor ❤️">
+        <img src="https://img.shields.io/static/v1?label=Sponsor&message=❤️&logo=github-sponsors&color=lightgrey&style=for-the-badge" alt="Sponsor ❤️">
       </a>
     </div>
-  </div>
+  </section>
 
   <section class="section">
     <div class="section-label">天象</div>
